361a34659a5a136ba635d9a1e92ce7c8edf85d86
EC2 Default User <ec2-user@ip-172-31-35-33.ap-northeast-1.compute.internal>
2023-08-02 10:50:23 +0000

    add post_index

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -2,13 +2,29 @@ class Public::PostsController < ApplicationController
   def new
     @post = Post.new
   end
+  
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
 
   def index
+    @posts = Post.all
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:post_image, :shop_name, :dish_name, :caption)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   belongs_to :user
   
   has_one_attached :post_image
+  
+  validates :post_image, presence: true
+  validates :shop_name, presence: true
+  validates :dish_name, presence: true
+  validates :caption, presence: true, length: { maximum: 100 }
+  
+  def get_post_image
+    (post_image.attached?) ? post_image : 'no_image.jpg'
+  end
 end
