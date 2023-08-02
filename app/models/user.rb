532b076954f82e7ce7ec2f0c1a5c00d7d521ca78
EC2 Default User <ec2-user@ip-172-31-35-33.ap-northeast-1.compute.internal>
2023-08-02 03:45:09 +0000

    add user_function

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_one_attached :user_image
+  has_many :relationships, foreign_key: :following_id
+  has_many :followings, through: :relationships, source: :follower
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: :follower_id
+  has_many :followers, through: :reverse_of_relationships, source: :following
+  has_one_attached :profile_image
+  
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :introduction, length: { maximum: 50 }
+  
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  
+  def following?(user)
+    followings.include?(user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :public do
+    get 'relationships/followings'
+    get 'relationships/followers'
+  end
 # 顧客用
 devise_for :users,skip: [:passwords], controllers: {
   registrations: "public/registrations",
@@ -12,12 +16,15 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :admin do
     root to: "posts#index"
-   
    end
 
    scope module: :public do
     root to: "posts#index"
     resources :posts
-    
+    resources :users, except: [:new] do
+      resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+    	get 'followers' => 'relationships#followers', as: 'followers'
     end
+  end
 end
