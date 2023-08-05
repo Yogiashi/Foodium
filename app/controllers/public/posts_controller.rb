f733a90405b1a38e3958b5286b042e70c1439f5b
EC2 Default User <ec2-user@ip-172-31-35-33.ap-northeast-1.compute.internal>
2023-08-05 22:31:24 +0000

    update layout

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -9,7 +9,7 @@ class Public::PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     if @post.save
-      redirect_to posts_path
+      redirect_to posts_path, notice: "投稿に成功しました。"
     else
       render :new
     end
@@ -32,7 +32,7 @@ class Public::PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     if @post.update(post_params)
-      redirect_to post_path(@post)
+      redirect_to post_path(@post), notice: "投稿内容を更新しました。"
     else
       render "edit"
     end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,23 +1,25 @@
 class Post < ApplicationRecord
-  
+
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_one :notification, as: :subject, dependent: :destroy
   has_one_attached :post_image
-  
+
   validates :post_image, presence: true
   validates :shop_name, presence: true
   validates :dish_name, presence: true
   validates :caption, presence: true, length: { maximum: 200 }
-  
+  validates :price, presence: true
+  validates :address, presence: true
+
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
-  
+
   def get_post_image
     (post_image.attached?) ? post_image : 'no_image.jpg'
   end
-  
+
   def liked?(user)
      likes.where(user_id: user.id).exists?
   end
