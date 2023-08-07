7f874a76fa5c6e221fa42fd7ca6f25817901dfa4
EC2 Default User <ec2-user@ip-172-31-35-33.ap-northeast-1.compute.internal>
2023-08-07 17:19:35 +0000

    add search_form_slide

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :post_tags,dependent: :destroy
-  has_many :tags,through: :post_tags
+  has_many :tags,through: :post_tags, dependent: :destroy
   has_one :notification, as: :subject, dependent: :destroy
   has_one_attached :post_image
 
