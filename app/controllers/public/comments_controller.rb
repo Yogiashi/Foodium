bcf1cbd8268f3391934163c2db2d9234d03d7d2f
EC2 Default User <ec2-user@ip-172-31-35-33.ap-northeast-1.compute.internal>
2023-08-30 15:51:11 +0000

    update comment_count_ajax

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,25 +1,29 @@
 class Public::CommentsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
     @post = Post.find(params[:post_id])
     @comments = @post.comments.all
   end
-  
+
   def create
     @post = Post.find(params[:post_id])
-     @comments = @post.comments.all
+    @comments = @post.comments.all
     comment = current_user.comments.new(comment_params)
     comment.post_id = @post.id
     comment.save
+    # フォローしているユーザーの公開中の投稿を取得
+    @following_posts = Post.where(user_id: [*current_user.following_ids], displayed: true).page(params[:following_page]).per(12).order(created_at: :desc)
   end
-  
+
   def destroy
     @post = Post.find(params[:post_id])
     @comments = @post.comments.all
     Comment.find(params[:id]).destroy
+    # フォローしているユーザーの公開中の投稿を取得
+    @following_posts = Post.where(user_id: [*current_user.following_ids], displayed: true).page(params[:following_page]).per(12).order(created_at: :desc)
   end
-  
+
   private
   def comment_params
     params.require(:comment).permit(:content)
